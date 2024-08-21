fcdce251f3c0c3d2c6bbc3d90f0b7caba8538493
Ekanshmwr <101271986+Ekanshmwr@users.noreply.github.com>
2024-08-21 18:15:29 +0530

    Create Intrest Amount Calculator.py

---
+++ Day 2/Intrest Amount Calculator.py
@@ -0,0 +1,7 @@
+amount = float(input("Enter the given amount: "))
+interest_rate = float(input("Enter the interest rate (%): "))
+
+interest_amount = (amount * interest_rate) / 100
+print(f"Given amount: {amount}")
+print(f"Interest rate: {interest_rate}%")
+print(f"Interest amount: {interest_amount}")
