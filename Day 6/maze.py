69fe989dbc5a516771f110b2f4f803fdf48440be
Ekanshmwr <101271986+Ekanshmwr@users.noreply.github.com>
2024-08-26 16:56:27 +0530

    Create maze.py

---
+++ Day 6/maze.py
@@ -0,0 +1,14 @@
+while front_is_clear():
+      move()
+      turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_left()
+        turn_left()
+        turn_left()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
